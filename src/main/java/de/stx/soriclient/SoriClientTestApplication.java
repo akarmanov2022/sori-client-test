5ad965babe7b4f2dd635372d9f5c2945b49b5a4c
aka <aka@startext.ru>
2022-06-20 12:16:45 +0700

    Initial commit

---
+++ src/main/java/de/stx/soriclient/SoriClientTestApplication.java
@@ -0,0 +1,13 @@
+package de.stx.soriclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoriClientTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SoriClientTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/stx/soriclient/SoriClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stx.soriclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoriClientTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
